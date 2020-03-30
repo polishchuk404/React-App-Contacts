0f21448eceff4e642db8466fd1714170a21077a8
Vadim <polish4uck@gmail.com>
2020-03-30 18:22:46 +0300

    Updates

---
+++ precache-manifest.47e2dd9807ef27ce9519287ad2ce0f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d856e7352ac0b537eda7af3620fa954",
+    "url": "/React-App-Contacts/index.html"
+  },
+  {
+    "revision": "dbbbbbd1e9f7384c0e96",
+    "url": "/React-App-Contacts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89a86d63f19ddfe04aee",
+    "url": "/React-App-Contacts/static/js/2.7aa66136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Contacts/static/js/2.7aa66136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbbbbd1e9f7384c0e96",
+    "url": "/React-App-Contacts/static/js/main.0b87a8a2.chunk.js"
+  },
+  {
+    "revision": "f0d6f8db73757f9d56eb",
+    "url": "/React-App-Contacts/static/js/runtime-main.563ed1ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-App-Contacts/static/media/logo.5d5d9eef.svg"
+  }
+]);
